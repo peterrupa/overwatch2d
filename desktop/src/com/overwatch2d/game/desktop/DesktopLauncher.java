a7e9b5b922f300c607c340ad6e1acbcc224c1082
Peter Rupa <megatricycle@gmail.com>
2016-11-04 21:03:47 +0800

    Initial Commit

---
+++ desktop/src/com/overwatch2d/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.overwatch2d.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.overwatch2d.game.Overwatch2D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Overwatch2D(), config);
+	}
+}
